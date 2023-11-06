744be935a3df5b613b588694a5cd28cab2198c71
assafpin <assaf.pinhasi@gmail.com>
2023-11-06 15:52:10 +0200

    small fixes

--- execution/py_tester_bindings/make_python_bindings.sh
+++ execution/py_tester_bindings/make_python_bindings.sh
@@ -2,6 +2,7 @@
 
 PYBIN="/usr/bin/python3.9"
 PYLIB="/usr/lib/python3.9"
+rm -rf wheelhouse
 export PYTHON_BIN_PATH=${PYBIN} && export PYTHON_LIB_PATH=${PYLIB}
 $PYBIN -m pip install -r execution/py_tester_bindings/requirements.txt
 rm -f bazel-bin/execution/py_tester_bindings/py_tester_extention.so
@@ -10,12 +11,17 @@ bazel build //execution/py_tester_bindings:py_tester_extention.so --
 sudo cp bazel-bin/execution/py_tester_bindings/py_tester_extention.so execution/py_tester_bindings/code_contests_tester/
 
 
-pybind11-stubgen execution/py_tester_bindings/py_tester_extention.so
+cd execution/py_tester_bindings
+
+pybind11-stubgen code_contests_tester
+
+cd ../../
 
 $PYBIN -m  pip wheel execution/py_tester_bindings 	--no-deps -w wheelhouse/
 
 #python setup.py sdist bdist_wheel
-$PYBIN -m pip pip install wheelhouse/code_contests_tester-0.1-py3-none-any.whl --force-reinstall
-cp execution/py_tester_bindings/test_python_bindings.py .
+$PYBIN -m pip install wheelhouse/*.whl --force-reinstall
+mkdir test
+cp execution/py_tester_bindings/test_python_binding.py test/
 
-$PYBIN  test_python_bindings.py /usr/bin/python3.9 /usr/lib/python3.9
+$PYBIN test/test_python_binding.py /usr/bin/python3.9 /usr/lib/python3.9
